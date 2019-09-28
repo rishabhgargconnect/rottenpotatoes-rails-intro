16551c8030a796e3b15b85c11a552840c3d204d2
EC2 Default User <ec2-user@ip-172-31-46-209.us-east-2.compute.internal>
2019-09-28 18:36:36 +0000

    hw2- without boostrap

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-
+    
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -11,7 +11,50 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+
+    #Storing all unique ratings from table
+    @all_ratings=Movie.uniq.pluck(:rating)
+    
+    sort_column = params[:sort_column]
+    
+    #Below is via session else default sort column implementation
+    if(!sort_column and session[:sort_column])
+      sort_column = session[:sort_column]
+    else  
+      sort_column ||='release_date'
+    end
+    
+    session[:sort_column] = sort_column
+    
+    sort_direction = params[:sort_direction]
+    
+    #Below is via session else default sort direction implementation
+    if(!sort_direction and session[:sort_direction])
+      sort_direction = session[:sort_direction]
+    else
+      sort_direction ||= "desc"
+    end
+    session[:sort_direction] = sort_direction
+    
+    @ratings_selected = params[:ratings]
+    
+    if(@ratings_selected)
+      @ratings_selected_keys = @ratings_selected.keys
+    end
+    
+    #Below is via session else default ratings(ie:all) implementation
+    if(!@ratings_selected and session[:ratings] )
+      @ratings_selected = session[:ratings]
+      @ratings_selected_keys = session[:ratings].keys
+    else  
+      @ratings_selected_keys||=@all_ratings
+    end
+    
+    session[:ratings] = @ratings_selected
+    
+    #Retrieve the filtered columns ordered by the given column and given direction.
+    @movies = Movie.where(rating: @ratings_selected_keys).order(sort_column +" "+ sort_direction)
+
   end
 
   def new
@@ -40,6 +83,10 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def destroy2
+    @movie = Movie.find(params[:id])
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,43 @@
 module ApplicationHelper
+    
+    #Method to highliught the appropriate sorted field
+    def css_helper(sort_column)
+        if(params[:sort_column]==sort_column)
+            return 'hilite'
+        end
+        if(session[:sort_column]==sort_column)
+            return 'hilite'
+        end
+    end
+    
+        #Method to highliught the appropriate sorted field
+    def css_helper(sort_column)
+    return '.bg-warning'
+    end
+    
+    #Method to sort by move title or Release date
+    def sort_helper(sort_column)
+        
+        #Set sort direction
+        sort_direction = params[:sort_direction]
+        if(sort_direction=="asc")
+            sort_direction="desc"
+        else
+            sort_direction = "asc"
+        end
+        
+        #Set Display string
+        link_display = ''
+        if(sort_column=='title')
+            link_display = 'Movie Title'
+        elsif(sort_column == 'release_date')
+            link_display = 'Release Date'
+        end
+        if(!@ratings_selected)
+            @ratings_selected = session[:ratings]
+        end
+        
+        link_to link_display, :sort_column => sort_column, :sort_direction => sort_direction, :ratings => @ratings_selected
+    end
+    
 end
